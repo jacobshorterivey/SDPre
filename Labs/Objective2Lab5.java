1a38c46f7167334b8dffeedec1617e97d0ef2f7a
Jacob <jacobshorterivey@gmail.com>
2019-08-25 16:36:39 -0600

    finishedO2L5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,11 @@
+public class Objective2Lab5{
+  public static void main(String[] args){
+    double side1, side2, hypotenuse;
+
+    side1 = 4;
+    side2 = 9;
+    hypotenuse = Math.sqrt((side1*side1)+(side2*side2));
+
+    System.out.println("If side 1 is " + side1 + " and side 2 is " + side2 + " then, the hypotenuse is " + hypotenuse + ".");
+  }
+}
