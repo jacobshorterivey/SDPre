e82846c94de3b91d3005e9a0c8397f68d35fd75a
Jacob <jacobshorterivey@gmail.com>
2019-08-25 16:06:16 -0600

    finished O2L3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "curry";
+    food2 = "french fries";
+    food3 = "clam chowder";
+
+    System.out.println("My Favorite Foods Are: " + food1 + ", " + food2 + ", and " + food3 + ".");
+  }
+}
