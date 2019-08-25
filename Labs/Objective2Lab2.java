c68982e8e4badb8cd20fbd05cb1c287aa6c4dd41
Jacob <jacobshorterivey@gmail.com>
2019-08-25 15:29:28 -0600

    finished O2L2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jacob Shorter-Ivey";
+    int age = 27;
+    String state = "California";
+
+    System.out.println("My name is " + name + ". I'm " + age + ", and I'm from " + state + ".");
+  }
+}
