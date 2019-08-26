e8e81d4cabc52777e652278e0e4cc12f18437309
Jacob <jacobshorterivey@gmail.com>
2019-08-26 14:49:02 -0600

    finished ex15d3

--- JavaTheHardWay/AgeMessages.java
+++ JavaTheHardWay/AgeMessages.java
@@ -12,21 +12,40 @@ public class AgeMessages {
     if ( age < 13 ) {
       System.out.println( "\ttoo young to create a Facebook account" );
     }
+    if ( age > 13 ) {
+      System.out.println( "\told enough to create a Facebook account" );
+    }
     if ( age < 16 ) {
       System.out.println( "\ttoo young to get a driver's license" );
+    }
+    if ( age > 16 ) {
+      System.out.println( "\told enough to get a driver's license" );
     }
     if ( age < 18 ) {
       System.out.println( "\ttoo young to get a tattoo" );
     }
+    if ( age > 18 ) {
+      System.out.println( "\told enough to get a tattoo" );
+    }
     if ( age < 21 ) {
       System.out.println( "\ttoo young to drink alcohol" );
+    }
+    if ( age > 21 ) {
+      System.out.println( "\told enough to drink alcohol" );
     }
     if ( age < 35 ) {
       System.out.println( "\ttoo young to run for President of the U.S." );
       System.out.println( "\t\t(How sad!)" );
     }
+    if ( age > 35 ) {
+      System.out.println( "\told enough to run for President of the U.S." );
+      System.out.println( "\t\t(Dope!)" );
+    }
+    if (age < 65 ) {
+      System.out.println( "\ttoo young to retire!" );
+    }
     if (age > 65 ) {
-      System.out.println( "\tYou are old enough to retire!" );
+      System.out.println( "\told enough to retire!" );
     }
   }
 }
