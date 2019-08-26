f5e816c4ce336880265b3daaaa985f505ddb9863
Jacob <jacobshorterivey@gmail.com>
2019-08-26 13:03:38 -0600

    finished ex12d1

--- JavaTheHardWay/BooleanExpressions.java
+++ JavaTheHardWay/BooleanExpressions.java
@@ -32,5 +32,13 @@ public class BooleanExpressions {
     System.out.println( !(x != y) + " " + (x == y) );
     System.out.println( !(x > y) + " " + (x <= y) );
     System.out.println( !(x >= y) + " " + (x < y) );
+
+    // less than is <
+    // greater than is >
+    // less than or equal to is <=
+    // greater than or equal to is >=
+    // equal to is ==
+    // not equal to is !=
+
   }
 }
