e11680ca9eb99f6dad141103320407dc2a3c871f
Jacob <jacobshorterivey@gmail.com>
2019-08-26 15:11:03 -0600

    finished ex16

--- JavaTheHardWay/ComparingNumbers.java
+++ JavaTheHardWay/ComparingNumbers.java
@@ -10,22 +10,24 @@ public class ComparingNumbers {
     System.out.print( "Second: " );
     second = keyboard.nextDouble();
 
-    if ( first < second ) {
+    if ( first < second ) 
       System.out.println( first + " is LESS THAN " + second );
-    }
-    if ( first <= second ) {
+
+    if ( first <= second )
       System.out.println( first + " is LESS THAN/EQUAL TO " + second );
-    }
-    if ( first == second ) {
+
+    if ( first == second )
       System.out.println( first + " is EQUAL TO " + second );
-    }
-    if ( first >= second ) {
+
+    if ( first >= second )
       System.out.println( first + " is GREATER THAN/EQUAL TO " + second );
-    }
-    if ( first > second ) {
+
+    if ( first > second )
       System.out.println( first + " is GREATER THAN " + second );
-    }
-    if ( first != second )
+
+    if ( first != second ){
       System.out.println( first + " is NOT EQUAL TO " + second );
+      System.out.println( "Hey." );
+    }
   }
 }
