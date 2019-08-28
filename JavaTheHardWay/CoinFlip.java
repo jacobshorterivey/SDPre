7307f69cc63484b260e2d60bdd73aa19bc2f9ffc
Jacob <jacobshorterivey@gmail.com>
2019-08-28 17:35:43 -0600

    finished ex29

--- JavaTheHardWay/CoinFlip.java
+++ JavaTheHardWay/CoinFlip.java
@@ -7,7 +7,60 @@
       int streak = 0;
       boolean gotHeads;
 
-      do {
+      gotHeads = Math.random() < 0.5;
+
+      if ( gotHeads )
+        coin = "HEADS";
+      else
+        coin = "TAILS";
+
+        System.out.println("You flip a coin and it is..." + coin);
+
+        if ( gotHeads ) {
+          streak++;
+          System.out.println("\tThat's " + streak + " in a row....");
+          System.out.print("\tWould you like to flip again (y/n)? ");
+          again = keyboard.next();
+        }
+        else {
+          System.out.println("\tYou lose everything!");
+          System.out.println("\tShould've quit while you were aHEAD....");
+          streak = 0;
+          again = "n";
+        }
+
+      while ( again.equals("y") ){
+        gotHeads = Math.random() < 0.5;
+
+        if ( gotHeads )
+          coin = "HEADS";
+        else
+          coin = "TAILS";
+
+          System.out.println("You flip a coin and it is..." + coin);
+
+          if ( gotHeads ) {
+            streak++;
+            System.out.println("\tThat's " + streak + " in a row....");
+            System.out.print("\tWould you like to flip again (y/n)? ");
+            again = keyboard.next();
+          }
+          else {
+            System.out.println("\tYou lose everything!");
+            System.out.println("\tShould've quit while you were aHEAD....");
+            streak = 0;
+            again = "n";
+      }
+    }
+    System.out.println( "Final score: " + streak );
+  }
+}
+
+
+
+
+
+    /*  do {
         gotHeads = Math.random() < 0.5;
 
         if ( gotHeads )
@@ -32,5 +85,5 @@
         } while ( again.equals("y") );
 
           System.out.println( "Final score: " + streak );
-      }
-    }
+
+          */
