f1f5febd98bc3612007869751637f8efce373a50
Jacob <jacobshorterivey@gmail.com>
2019-08-28 15:11:46 -0600

    finishedex24d1

--- JavaTheHardWay/RandomNumbers.java
+++ JavaTheHardWay/RandomNumbers.java
@@ -61,5 +61,10 @@ public class RandomNumbers {
     System.out.println( "0-9.99:\t" + r );
     System.out.println( "0-9:\t" + (int)r );
     System.out.println( "1-10:\t" + (1 + (int)r) );
+
+    a = 1 + (int)( 3*Math.random() );
+    b = 1 + (int)( 3*Math.random() );
+    c = 1 + (int)( 3*Math.random() );
+    System.out.println( "1-3:\t" + a + "\t" + b + "\t" + c );
   }
 }
